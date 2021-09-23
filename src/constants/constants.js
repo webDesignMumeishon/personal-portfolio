c1df6a194d0a8814e094017bcd7886fddc175804
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-09-23 18:17:04 -0300

    projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,39 +1,21 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 0,
-  },
-  {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
-  },
-  {
     title: 'Movie App',
     description: "The project consists of calling an API (http://www.omdbapi.com/). The user types the movie in the search bar, after clicking the API is called and a set of movies in the main page are shown with details displayed, to see further details the user have to click over the name. Every single movie can be added to the favorite sections.",
       image: '/images/movie-app.png',
       tags: ['React', 'Redux', "AJAX"],
     source: 'https://github.com/webDesignMumeishon/movie-app',
     visit: 'https://movie-app-mumeishon.netlify.app/',
-    id: 2,
+    id: 0,
   },
   {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
+    title: 'books-counter-app',
+    description: "This app was built behind the idea of the data structure STACK. A stack is an abstract data type that holds an ordered, linear sequence of items. In contrast to a queue, a stack is a last in, first out (LIFO) structure. It was developed only using plain vanilla JavaScript, HTML and CSS.",
+    image: '/images/book-stack.png',
+    tags: ['Javascript', 'HTML', 'CSS'],
+    source: 'https://github.com/webDesignMumeishon/books-counter-app',
+    visit: 'https://webdesignmumeishon.github.io/books-counter-app/',
+    id: 1,
   },
 ];
 
@@ -41,5 +23,4 @@ export const TimeLineData = [
   { year: 2020, text: 'Started my journey', },
   { year: 2021, text: 'Got into a Henry Bootcamp', },
   { year: 2021, text: 'Currently looking for my first job as a developer', },
-  // { year: 2021, text: 'Started my own platform', },
 ];
