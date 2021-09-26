485311270992c47ab0903cc02a8b9613cd244742
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-09-26 11:46:01 -0300

    projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,21 @@
 export const projects = [
   {
+    title: 'videogames-app',
+    description: "The projectconsists of calling an API (https://rawg.io/apidocs) to show an initial set of 80 videogames on the /home page to see a variety of items and click over oneand to access the details about the platforms, genres, name, released date, etc.The app has a database built with Sequelize and Postgres where the client canadd new games by filling up a form. For the front-end was developed using React and Redux. ",
+    image: '/images/videogame-app.png',
+    tags: ['Sequelize', 'Postgres', "React", "Express"],
+    source: 'https://github.com/webDesignMumeishon/videogames-main',
+    visit: 'https://videogames-app-pi.netlify.app/',
+    id: 0,
+  },
+  {
     title: 'find-henry-app',
-    description: "The app consists in a map where all the registered and verified users can be seen in the location that was selected by them. The app was meant to have all the henry bootcamp’s graduates connected across the world. The back-end was developed using MongoDB & Mongoose for the database, and Express. React.js for client side. Also, the site has its own admin page. A login system was implemented together with a reset password.",
+    description: "The app consists of a map where all the registered and verified users can be seen in the location that was selected by them. The app was meant to have all the henry bootcamp’s graduates connected across the world. The back-end was developed using MongoDB & Mongoose for the database, and Express. React.js for client side. Also, the site has its own admin page. A login system was implemented together with a reset password.",
     image: '/images/find-henry-app.png',
     tags: ['MongoDB', 'Mongoose', "React", "Express"],
     source: 'https://github.com/webDesignMumeishon/red-henry',
     visit: 'https://find-closest-henry.netlify.app/',
-    id: 0,
+    id: 1,
   },
   {
     title: 'Movie App',
@@ -15,7 +24,7 @@ export const projects = [
       tags: ['React', 'Redux', "AJAX"],
     source: 'https://github.com/webDesignMumeishon/movie-app',
     visit: 'https://movie-app-mumeishon.netlify.app/',
-    id: 1,
+    id: 2,
   },
   {
     title: 'books-counter-app',
@@ -24,7 +33,16 @@ export const projects = [
     tags: ['Javascript', 'HTML', 'CSS'],
     source: 'https://github.com/webDesignMumeishon/books-counter-app',
     visit: 'https://webdesignmumeishon.github.io/books-counter-app/',
-    id: 2,
+    id: 3,
+  },
+  {
+    title: 'weather-app',
+    description: "The app was developed during the bootcamp course. It was the first project built after learning React.The app consists in calling an external API that provides information about the current weather of any city in the world. The cities get accumulated in several cards in the cards section.The technologies used are React.js, JavaScript, HTML and CS",
+    image: '/images/weather-app.png',
+    tags: ['Javascript', 'HTML', 'CSS', 'React'],
+    source: 'https://github.com/webDesignMumeishon/weather-app',
+    visit: 'https://weather-app-tomasmacchi.netlify.app/',
+    id: 4,
   },
 ];
 
